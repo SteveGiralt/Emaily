bf75001aac2367a5145d07b7dab563b5b7be3321
Steve Giralt <steve.giralt@gmail.com>
2022-03-13 22:24:29 -0600

    Setup Redux store, begin auth flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper teal lighten-2">
+          <a href="/" className="brand-logo left">
+            Emaily
+          </a>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
