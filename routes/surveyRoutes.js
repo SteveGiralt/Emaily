afa061b2dff2bec7b79d8dbdc25d8c7420c7eded
Steve Giralt <steve.giralt@gmail.com>
2022-03-19 22:47:01 -0600

    sendgrid setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -24,6 +24,16 @@ module.exports = (app) => {
         dateSent: Date.now(),
       });
       const mailer = new Mailer(survey, surveyTemplate(survey));
+      try {
+        await mailer.send();
+        await survey.save();
+        req.user.credits -= 1;
+        const user = await req.user.save();
+        res.send(user);
+      } catch (err) {
+        res.status(422);
+        res.send(err);
+      }
     });
   app.post("/api/surveys/webhooks", (req, res) => {});
 };
